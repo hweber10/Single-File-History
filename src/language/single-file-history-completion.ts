42752eaf53fc3bde5414902eb30b3253cf1b107f
hweber <Hannes.Weber@vector.com>
2025-01-16 10:42:52 +0100

    editing language to be yaml-conform

--- src/language/single-file-history-completion.ts
+++ src/language/single-file-history-completion.ts
@@ -2,7 +2,7 @@ import { CompletionItemKind, MarkupKind } from "vscode-languageserver"
 import { CompletionAcceptor, NextFeature, CompletionContext } from "langium/lsp";
 import { DefaultCompletionProvider } from "langium/lsp";
 
-export const exampleFile = 'Version: 1.0\nProject:\n    CBD-Number: "CBD1"\n    PRJ-Number: "PRJ1"\n    Title: ""\n    SDP-Version: 1.0\nHistory:\n- ID: "H1"\n    Sub-Project: ""\n    Del: "D1"\n    Author: ""\n    Date: 0000-00-00\n    Topic: ""\n    Description: ""\n    Type: "Task"\n    Duedate: 0000-00-00\n    Resp: ""\n    State: "Closed"';
+export const exampleFile = 'Version: 1.0\nProject:\n    CBD-Number: "CBD1"\n    PRJ-Number: "PRJ1"\n    Title: ""\n    SDP-Version: 1.0\nHistory:\n  - ID: "H1"\n    Sub-Project: ""\n    Del: "D1"\n    Author: ""\n    Date: 0000-00-00\n    Topic: ""\n    Description: ""\n    Type: "Task"\n    Duedate: 0000-00-00\n    Resp: ""\n    State: "Closed"';
 const exampleHistory = 'Sub-Project: ""\n    Del: "D1"\n    Author: ""\n    Date: 0000-00-00\n    Topic: ""\n    Description: ""\n    Type: "Task"\n    Duedate: 0000-00-00\n    Resp: ""\n    State: "Closed"';
 
 export class SingleFileHistoryCompletionProvider extends DefaultCompletionProvider {
@@ -24,7 +24,7 @@ export class SingleFileHistoryCompletionProvider extends DefaultCompletionProvid
                 label: ("addHistory"),
                 labelDetails: {description: "generates a new History"},
                 kind: CompletionItemKind.Keyword,
-                insertText: '\n- ID: "H'+(Number(context.node.$containerIndex)+2)+'"\n    '+exampleHistory,
+                insertText: '\n  - ID: "H'+(Number(context.node.$containerIndex)+2)+'"\n    '+exampleHistory,
                 insertTextMode: 1,
                 documentation: {kind: MarkupKind.Markdown, value: "This Code-Snippet automatically generates a default **History**."},
             })
